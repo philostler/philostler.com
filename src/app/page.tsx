3fbfb7f5cc309f7d4eed4da0a3bb533cc921b255
Phil Ostler <github@philostler.com>
2024-02-15 11:06:46 +0000

    feat: add name header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 import Image from "next/image";
 import clsx from "clsx";
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 const page = () => (
-  <main className="flex flex-col items-center justify-between min-h-screen m-24">
+  <main className="flex flex-col items-center min-h-screen m-24">
+    <div
+      className={clsx(
+        ubuntu.className,
+        "backdrop-blur-xl",
+        "bg-slate-50/70",
+        "dark:bg-zinc-950/70",
+        "fixed",
+        "p-4",
+        "shadow-sm",
+        "text-center",
+        "top-0",
+        "w-full",
+        "flex",
+      )}
+    >
+      <h1 className={clsx("drop-shadow", "text-xl")}>Phil Ostler</h1>
+    </div>
     <Image
       alt="Phil Ostler's Profile"
       className={clsx(
